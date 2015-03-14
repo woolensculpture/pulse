6677cf9d5ba9b069b62c1d040489bdf3b9045a4b
Eli <ewc3304@rit.edu>
2015-03-14 19:02:33 +0000

    Show stuff, fix files later

--- app/Show.php
+++ app/Show.php
@@ -5,10 +5,19 @@ use Illuminate\Database\Eloquent\Model;
 class Show extends Model {
 
 	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'shows';
+
+	/**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
 	 */
 	protected $fillable = ['name', 'description', 'show_picture', 'slider_picture', 'active', 'style'];
+
+	public $timestamps = false;
 
 }
