9f1364730f0406bd1483484c54d6444bd3f4d095
John Betley <jpbetley@gmail.com>
2015-04-12 18:19:46 -0400

    auth-buttons fully operation. You may fire when ready.

--- app/ViewComposers/AuthViewComposer.php
+++ app/ViewComposers/AuthViewComposer.php
@@ -2,16 +2,22 @@
 
 namespace WITR\ViewComposers;
 
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Contracts\Auth\Guard;
+use WITR\Services\Whitelist;
 
 class AuthViewComposer {
 
 	protected $auth;
+	protected $whitelist;
+	protected $request;
 
-	public function __construct(Guard $auth)
+	public function __construct(Guard $auth, Whitelist $whitelist, Request $request)
 	{
 		$this->auth = $auth;
+		$this->whitelist = $whitelist;
+		$this->request = $request;
 	}
 
 	/**
@@ -23,6 +29,7 @@ class AuthViewComposer {
 	public function compose(View $view)
 	{
 		$view->with('user', $this->auth->user())
-			 ->with('authenticated', $this->auth->check());
+			 ->with('authenticated', $this->auth->check())
+			 ->with('in_station', $this->whitelist->inRange($this->request));
 	}
 }
