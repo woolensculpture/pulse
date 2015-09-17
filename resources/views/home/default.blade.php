860b2ad65daa344772f763914cb17c77f27650e9
Eli Clampett <ewc3304@rit.edu>
2015-09-17 09:13:14 -0500

    Update default.blade.php

--- resources/views/home/default.blade.php
+++ resources/views/home/default.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <div id="home">
-	<img src="{{ secure_asset('img/default.jpg') }}" alt="ZOMG! It's Dan Mansen!" align="middle">
+	<img src="{{ secure_asset('img/default.jpg') }}" alt="ZOMG! It's Dan Mansen!" style="margin-left: 12px">
 </div>
 @stop
