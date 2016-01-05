a269fa8c468effe68f37d2363a3d7eb1acc70745
Eli <ewc3304@rit.edu>
2016-01-05 01:11:37 +0000

    Removing non .blade.php files

--- resources/views/askdestlernexttime.php
+++
@@ -1,5 +0,0 @@
-<div class="border general_wrap">
-	<div class="header">Ask Destler</div>
-	<hr class="white" />
-	<h4>Thanks for listening! Keep tuning in to The Pulse of Music on WITR 89.7 to find out when Dr. Destler will return!</h4>
-</div>
--- resources/views/main/generic_response.php
+++
@@ -1,1 +0,0 @@
-<?php echo $generic_response; ?>
